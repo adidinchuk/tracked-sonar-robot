39a4d4c77cc379d0fd8684701d805b9dd98691aa
Andrew <adidinchuk@gmail.com>
2023-03-06 17:50:19 -0500

    Added robot object and velocity motion model simulator

---
+++ motion_control.py
@@ -0,0 +1,95 @@
+from motor_control import MotorControl
+import RPi.GPIO as GPIO  
+
+class MotionControl:
+
+    def __init__(self, enable_pin_left=25, forward_pin_left=23, reverse_pin_left=24, \
+    enable_pin_right=20, forward_pin_right=16, reverse_pin_right=12, min_power=50):
+        self.left_motor = MotorControl(GPIO, enable_pin_left, forward_pin_left, reverse_pin_left, min_power)
+        self.right_motor = MotorControl(GPIO, enable_pin_right, forward_pin_right, reverse_pin_right, min_power)
+
+    def forward(self, speed):
+        self.move(speed)
+        return True
+    
+    def reverse(self, speed):
+        self.move(-speed)
+        return True
+    
+    def rotate(self, direction, speed):
+        if(direction == 'left'):
+            self.left_motor.set_velocity(-speed)
+            self.right_motor.set_velocity(speed)
+        elif(direction == 'right'):        
+            self.left_motor.set_velocity(speed)
+            self.right_motor.set_velocity(-speed)
+        else:
+            return False
+        return True
+    
+    def stop(self):
+        self.left_motor.stop()
+        self.right_motor.stop()
+        return True
+
+    def move(self, speed):
+        self.left_motor.set_velocity(speed)
+        self.right_motor.set_velocity(speed)
+        return True
+    
+    def test_motors(self):
+        print("Runing test on left motor.")
+        self.left_motor.test()
+        print("Runing test on right motor.")
+        self.right_motor.test()
+
+if __name__ == '__main__':
+
+    position_control = MotionControl()
+
+    print("\n")
+    print("Running motor control test on default pins")
+    print("s-stop f-forward r-reverse l-left r-right t-test e-exit")
+    print("\n") 
+
+    while(1):
+
+        x=input()        
+
+        if x=='s':
+            print("stop")
+            position_control.stop()
+            x='z'
+
+        elif x=='f':
+            print("forward")
+            position_control.forward(75)
+            x='z'
+
+        elif x=='r':
+            print("reverse")
+            position_control.reverse(75)
+            x='z'
+        
+        elif x=='l':
+            print("left")
+            position_control.rotate(direction="left", speed=100)
+            x='z'
+        
+        elif x=='r':
+            print("right")
+            position_control.rotate(direction="right", speed=100)
+            x='z'
+        
+        elif x=='t':
+            print("testing")
+            position_control.test_motors()
+            x='z'
+
+        elif x=='e':
+            GPIO.cleanup()
+            break
+
+        else:
+            print("<<<  wrong data  >>>")
+            print("please enter the defined data to continue.....")
--- robot.py
+++ robot.py
@@ -1,91 +1,85 @@
-from motor_control import MotorControl
-import RPi.GPIO as GPIO  
+from datetime import datetime
+from motion_control import MotionControl
+from velocity_motion_model import VelocityMotionModel
+import numpy as np
+import time
 
-class Robot:
+class Robot():
+    #VELOCITY MAGNITUDES MUST BE IN UNIT/SECOND
 
-    def __init__(self, enable_pin_left=25, forward_pin_left=23, reverse_pin_left=24, \
-    enable_pin_right=20, forward_pin_right=16, reverse_pin_right=12, min_power=50):
-        self.left_motor = MotorControl(GPIO, enable_pin_left, forward_pin_left, reverse_pin_left, min_power)
-        self.right_motor = MotorControl(GPIO, enable_pin_right, forward_pin_right, reverse_pin_right, min_power)
-
-    def forward(self, speed):
-        self.move(speed)
-        return True
+    rotation_magnitude = 0
+    translation_magnitude = 0
+    model_timestamp = None
     
-    def reverse(self, speed):
-        self.move(-speed)
+
+    def __init__(self, track_spacing, initial_pose=np.array([[0,0,0]])):
+        self.pose = initial_pose
+        self.motion_controller = MotionControl()
+        self.track_spacing = track_spacing
+        #TODO set correct alphas
+        alphas = np.array([0.001, 0.001, 0.001, 0.001, 0.001, 0.001])
+        self.vmm = VelocityMotionModel(alphas, self.pose, sample_size=10)
+
+    def move_to_target(self, target_pose):
+        x_t, y_t, theta_t = target_pose
+        #TODO
+        print('move_to_target is not implemented.')
         return True
-    
-    def rotate(self, direction, speed):
-        if(direction == 'left'):
-            self.left_motor.set_velocity(-speed)
-            self.right_motor.set_velocity(speed)
-        elif(direction == 'right'):        
-            self.left_motor.set_velocity(speed)
-            self.right_motor.set_velocity(-speed)
+
+    def rotate(self, magnitude):
+        self.update_model()
+        self.rotation_magnitude = magnitude
+        if self.rotation_magnitude != 0:
+            self.motion_controller.rotate('right', self.rotation_magnitude)
+        elif self.rotation_magnitude > 0:
+            self.motion_controller.rotate('left', self.rotation_magnitude)
         else:
-            return False
+        #if self.rotation_magnitude == 0:
+            self.rotation_magnitude = 0
         return True
     
-    def stop(self):
-        self.left_motor.stop()
-        self.right_motor.stop()
+    def traverse(self, magnitude):
+        self.update_model()        
+        self.translation_magnitude = magnitude
+        if self.translation_magnitude > 0:
+            self.motion_controller.forward(self.translation_magnitude)
+        elif self.translation_magnitude < 0:
+            self.motion_controller.reverse(-self.translation_magnitude)
+        else:           
+        #if not self.translation_magnitude > 0:
+            self.translation_magnitude = 0
         return True
 
-    def move(self, speed):
-        self.left_motor.set_velocity(speed)
-        self.right_motor.set_velocity(speed)
-        return True
-    
-    def test_motors(self):
-        print("Runing test on left motor.")
-        self.left_motor.test()
-        print("Runing test on right motor.")
-        self.right_motor.test()
+    def stop(self):
+        self.update_model()
+        self.motion_controller.stop()
+        self.rotation_magnitude, self.translation_magnitude = 0, 0
+
+
+    def update_model(self):
+        if(self.translation_magnitude != 0 or self.rotation_magnitude != 0):
+            displacement = [self.translation_magnitude, (self.track_spacing / 2) * self.rotation_magnitude]
+            self.vmm.update(displacement, (datetime.now() - self.model_timestamp).total_seconds())
+        self.model_timestamp = datetime.now()
+
+    def graph_motion(self):
+        self.vmm.graph()
 
 if __name__ == '__main__':
-    robot = Robot()
 
-    print("\n")
-    print("Running motor control test on default pins")
-    print("s-stop f-forward r-reverse l-left r-right e-exit")
-    print("\n")  
+    robot = Robot(1)
 
-    robot.test_motors()
-
-    while(1):
-
-        x=input()        
-
-        if x=='s':
-            print("stop")
-            robot.stop()
-            x='z'
-
-        elif x=='f':
-            print("forward")
-            robot.forward(75)
-            x='z'
-
-        elif x=='r':
-            print("reverse")
-            robot.reverse(75)
-            x='z'
-        
-        elif x=='l':
-            print("left")
-            robot.rotate(direction="left", speed=100)
-            x='z'
-        
-        elif x=='r':
-            print("right")
-            robot.rotate(direction="right", speed=100)
-            x='z'
-
-        elif x=='e':
-            GPIO.cleanup()
-            break
-
-        else:
-            print("<<<  wrong data  >>>")
-            print("please enter the defined data to continue.....")
+    robot.traverse(10)
+    time.sleep(1)
+    #robot.update_model()
+    #time.sleep(0.5)
+    robot.stop()
+    time.sleep(0.1)
+    robot.rotate(1)
+    time.sleep(1)
+    robot.stop()
+    time.sleep(0.1)
+    robot.traverse(10)
+    time.sleep(1)
+    robot.stop()
+    robot.graph_motion()
