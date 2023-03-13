22d599924f0594df92c7f762a6b64d5218f48250
Andrew <adidinchuk@gmail.com>
2023-03-13 18:17:42 -0400

    adding MG996R module

--- sensors/mg996r.py
+++ sensors/mg996r.py
@@ -21,8 +21,8 @@ class MG996R():
 
 if __name__ == '__main__':
     pin = 3
-    low = 4.92
-    high = 9.778
+    low = 4.9
+    high = 9.77
     GPIO.setmode(GPIO.BOARD)
     print("Executing SG90 test on pin: " + str(pin))
     print("Low Duty Cycle threshold set to : " + str(low))
