5bc4ffeadb6223dbcbe4cd7d7c3b7b15a6db2980
Andrew <adidinchuk@gmail.com>
2023-03-13 18:16:22 -0400

    adding MG996R module

--- sensors/mg996r.py
+++ sensors/mg996r.py
@@ -31,13 +31,13 @@ if __name__ == '__main__':
 
     print("setting 0%...")
     sg90.update_angle(-180)
-    time.sleep(1)
+    time.sleep(3)
     print("setting 90%...")
     sg90.update_angle(0)
-    time.sleep(1)
+    time.sleep(3)
     print("setting 180%...")
     sg90.update_angle(180)
-    time.sleep(1)
+    time.sleep(3)
 
     print("Test complete...")
     GPIO.cleanup()
